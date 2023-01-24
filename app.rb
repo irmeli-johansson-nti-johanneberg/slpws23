51d924f870481ca608f49915992241d5ecb1bb80
irmeli-johansson-nti-johanneberg <irmeli.johansson@elev.ga.ntig.se>
2023-01-24 12:44:03 +0100

    index groups

--- app.rb
+++ app.rb
@@ -6,3 +6,16 @@ require 'bcrypt'
 
 enable :sessions
 
+get('/') do
+    slim(:start)
+end
+
+get('/group/') do
+
+   db = SQLite3::Database.new('db/slpws23.db')
+   db.results_as_hash = true
+   result = db.execute("SELECT * FROM group")
+
+    slim(:"group/index")
+    #slim(:"group/index", locals:{groups:result})
+end
